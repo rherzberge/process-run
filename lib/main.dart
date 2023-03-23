4d3762aa1d76da462247a5ba7f6c3e34333c3e48
rherzberge <105018280+rherzberge@users.noreply.github.com>
2023-03-23 05:22:29 -0400

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      theme: ThemeData(
+        colorSchemeSeed: Colors.indigo,
+        useMaterial3: true,
+        brightness: Brightness.light,
+      ),
+      darkTheme: ThemeData(
+        colorSchemeSeed: Colors.blue,
+        useMaterial3: true,
+        brightness: Brightness.dark,
+      ),
+      home: const MyHomePage(title: 'Flutter Example App'),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+  final controller = TextEditingController();
+  String stdin = '';
+  String stdout ='';
+  String result = '';
+  processInt()async{
+    var process = Process.start('am', [result]);
+    process.then((value){
+      setState(() {
+              stdin = value.stdin.toString();
+              stdout = value.stdout.toString();
+            });
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+         
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+           
+            Text(
+              '$stdin',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            SizedBox(height: 20),
+            Text(
+              '$stdout',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            SizedBox(height: 50),
+            TextField(controller: controller, onChanged: (value){
+              setState(() {
+                              result = value;
+                            });
+            },),
+            Spacer (),
+            
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed:  result.isEmpty ? null : (){
+
+              processInt();},
+       // tooltip: 'Increment',
+        child: const Icon(Icons.arrow_forward_ios),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
